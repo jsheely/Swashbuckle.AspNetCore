eac2ef5bab432f50e5203f1cb3acbe874c0c1ef4
Jonathan Sheely <jsheely@twentytech.net>
2017-12-09 18:26:33 -0500

    Added SwaggerDescription attribute Trying not to reuse DescriptionAttribute in case that is for other UI cases.

---
+++ src/Swashbuckle.AspNetCore.SwaggerGen/Annotations/SwaggerDescriptionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen
+{
+    
+    public class SwaggerDescriptionAttribute : Attribute
+    {
+        public SwaggerDescriptionAttribute(string description = null)
+        {
+            this.Description = description;
+        }
+
+        public string Description { get; set; }
+    }
+}
